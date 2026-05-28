3f7054224f34139a8359ce5eafb685632e7b8d63
Polina Yausheva <yausheva321@gmail.com>
2026-05-29 01:58:16 +0300

    step 6

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -17,12 +17,11 @@ module.exports = {
     dialect: 'sqlite',
     storage: './database.test.sqlite',
   },
-  // test: {
-  //   dialect: 'postgres',
-  //   database: process.env.DATABASE_NAME,
-  //   username: process.env.DATABASE_USERNAME,
-  //   password: process.env.DATABASE_PASSWORD,
-  //   port: process.env.DATABASE_PORT,
-  //   host: process.env.DATABASE_HOST,
-  // },
+  test: {
+    username: process.env.DATABASE_USERNAME || 'postgres',
+    password: process.env.DATABASE_PASSWORD || null,
+    database: process.env.DATABASE_NAME || 'postgres',
+    host: process.env.DATABASE_HOST || 'db',
+    dialect: 'postgres',
+  },
 };
